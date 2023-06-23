731e20b32134dcda72e6cbad3bb15f30af349661
talhakayani12 <talha.kayani@invozone.com>
2023-06-23 20:29:00 +0500

    Update Contract Address and Contract ABIs

--- contracts/config.js
+++ contracts/config.js
@@ -1,23 +1,23 @@
-const ethContractAbi = require("./ABIs/ethereum-contract-abi.json");
-const storageChainContractAbi = require("./ABIs/storagechain-contract-abi.json");
+const ethContractAbi = require('./ABIs/ethereum-contract-abi.json');
+const storageChainContractAbi = require('./ABIs/storagechain-contract-abi.json');
 
 const CONTRACT_DETAILS = {
   80001: {
     abi: ethContractAbi,
-    address: "0x616B297704922fCC9DFB399118F7F3E7f8513527",
+    address: '0xFacaF75a2D6bC89ab712ba471d353dB3389088aC',
   },
   8726: {
     abi: storageChainContractAbi,
-    address: "0xDc524101D7D3904cb37204b2b5470BFA4Cb11AF9",
+    address: '0x2A7b9b12e2fAeb99FfFF0765F2f484A8D3dFbEe7',
   },
 };
 
 const RPC_URLS = {
-  storagechain: "http://128.199.5.56:9933",
-  eth_goerli: "https://goerli.infura.io/v3/7a9d2a7b9c6f459ea3cddcd3a917c460",
+  storagechain: 'https://storchain-mainnet.invo.zone/',
+  eth_goerli: 'https://goerli.infura.io/v3/7a9d2a7b9c6f459ea3cddcd3a917c460',
   polygon_mombai:
-    "https://polygon-mumbai.infura.io/v3/7a9d2a7b9c6f459ea3cddcd3a917c460",
-  eth: "https://mainnet.infura.io/v3/7a9d2a7b9c6f459ea3cddcd3a917c460",
+    'https://polygon-mumbai.infura.io/v3/7a9d2a7b9c6f459ea3cddcd3a917c460',
+  eth: 'https://mainnet.infura.io/v3/7a9d2a7b9c6f459ea3cddcd3a917c460',
 };
 
 module.exports = { CONTRACT_DETAILS, RPC_URLS };
